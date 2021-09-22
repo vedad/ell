fixed double degree/radian conversion from a previous update

ell/orbit.py
@@ -58,7 +58,7 @@ class Orbit:
 #
     def _transform_to_orthogonal(self, x, y, l):
 
-        l = np.deg2rad(l)
+#        l = np.deg2rad(l)
 
         xn = x * np.cos(l) - y * np.sin(l)
         yn = x * np.sin(l) + y * np.cos(l)
@@ -68,7 +68,7 @@ class Orbit:
 
     def _rotate_around_x(self, x, y, z, i_star):
 
-        i_star = np.deg2rad(i_star)
+#        i_star = np.deg2rad(i_star)
         beta = 0.5*np.pi - i_star
 
         xrot = x

ell/surface.py
@@ -91,7 +91,7 @@ class Surface:
 
         ell    = np.deg2rad(ell)
         i_star = np.deg2rad(i_star)
-        beta   = 0.5 * np.pi - i_star # rotation around orthogonal x axis
+#        beta   = 0.5 * np.pi - np.deg2rad(i_star) # rotation around orthogonal x axis
         n      = len(x)
 
         # oversample grid

ell/utils/models.py
@@ -6,12 +6,15 @@ import sys
 
 def get_light_curve(x, period, t0, aor, ror, incl,
                     ustar=None, ld=None, ecc=0, omega=90, sbratio=0,
-                    oversample=1, texp=None):
+                    oversample=1, texp=None, **kwargs):
     try:
         from ellc import lc
     except ModuleNotFoundError:
         print("Please install `ellc` to use this function")
         sys.exit()
+
+    if not isinstance(x, np.ndarray):
+        x = np.array([x])
     
     r1oa = 1/aor
     r2oa = r1oa * ror
@@ -20,7 +23,7 @@ def get_light_curve(x, period, t0, aor, ror, incl,
 
     model = lc(x,
             radius_1=r1oa,
-            radius2=r2oa,
+            radius_2=r2oa,
             incl=incl,
             sbratio=sbratio,
             period=period,
@@ -31,16 +34,20 @@ def get_light_curve(x, period, t0, aor, ror, incl,
             ld_1=ld,
             ldc_1=ustar,
             t_exp=texp,
-            n_int=oversample)
+            n_int=oversample,
+            **kwargs)
+#            grid_1='very_sparse',
+#            grid_2='v)
 
     return model
 
-def get_radial_velocity(x, period, t0, incl, K, ecc=0, omega=90, sbratio=0):
+def get_radial_velocity(x, period, t0, incl, K, K2=None, ecc=0, omega=90, sbratio=0):
     try:
         from ellc import rv
     except ModuleNotFoundError:
         print("Please install `ellc` to use this function")
         sys.exit()
+
 
     K      *= u.m/u.s
     period *= u.day
@@ -48,8 +55,17 @@ def get_radial_velocity(x, period, t0, incl, K, ecc=0, omega=90, sbratio=0):
     a1 = (K * period * np.sqrt(1 - ecc**2) /
             (2*np.pi * np.sin(np.deg2rad(incl)))
             ).to(u.R_sun).value
-    q  = 1
-    a  = a1 * (1 + 1/q)
+
+    if K2 is not None:
+        K2 *= u.m / u.s
+        q   = K / K2
+        a2  = (K2 * period * np.sqrt(1 - ecc**2) /
+                 (2*np.pi * np.sin(np.deg2rad(incl)))
+                 ).to(u.R_sun).value
+        a   = a1 + a2
+    else:
+        q   = 1
+        a   = a1 * (1 + 1/q)
 
     fs = np.sqrt(ecc) * np.sin(np.deg2rad(omega))
     fc = np.sqrt(ecc) * np.cos(np.deg2rad(omega))
@@ -67,7 +83,11 @@ def get_radial_velocity(x, period, t0, incl, K, ecc=0, omega=90, sbratio=0):
             flux_weighted=False
             )
 
-    return model[0] * 1e3
+    if K2 is None:
+        return model[0] * 1e3
+    else:
+        return [m * 1e3 for m in model]
+
 
 def get_rossiter_mclaughlin(x, period, t0, aor, ror, incl, K, vsini, ell,
         ecc=0, omega=90, sbratio=0, ustar=None, ld=None,
@@ -92,7 +112,7 @@ def get_rossiter_mclaughlin(x, period, t0, aor, ror, incl, K, vsini, ell,
     r2oa = r1oa * ror
     f_s = np.sqrt(ecc) * np.sin(np.deg2rad(omega))
     f_c = np.sqrt(ecc) * np.cos(np.deg2rad(omega))
-    
+
     def _model(x, fw):
         return rv(x, 
                   radius_1=r1oa,
@@ -111,7 +131,9 @@ def get_rossiter_mclaughlin(x, period, t0, aor, ror, incl, K, vsini, ell,
                   vsini_1=vsini,
                   lambda_1=ell,
                   t_exp=texp,
-                  n_int=oversample
+                  n_int=oversample,
+                  grid_1='very_sparse',
+                  grid_2='very_sparse'
                   )[0] * 1e3
 
     if slope:

ell/utils/plots.py
@@ -423,9 +423,55 @@ def plot_limbdark_map(u=[0.4, 0.3], ld='quad', Nxy=200):
     return fig
 
 def plot_velocity_map(v_eq, ell, i_star=90, alpha=0, Nxy=200,
+                    u=None,
                     vmin=None,
                     vmax=None,
-                    planet=None):
+                    planet=None,
+                    figsize=(3.39, 3.39)
+                    ):
+
+
+#    inc = np.deg2rad(i_star)
+#    obl = np.deg2rad(ell)
+#    # Angular quantities
+#    ci = np.cos(inc)
+#    si = np.sin(inc)
+#    co = np.cos(obl)
+#    so = np.sin(obl)
+#
+#    # Latitude lines
+#    dlat=np.pi / 6
+#    res = []
+#    latlines = np.arange(-np.pi / 2, np.pi / 2, dlat)[1:]
+#    for lat in latlines:
+#
+#        # Figure out the equation of the ellipse
+#        y0 = np.sin(lat) * si
+#        a = np.cos(lat)
+#        b = a * ci
+#        x = np.linspace(-a, a, 1000)
+#        y1 = y0 - b * np.sqrt(1 - (x / a) ** 2)
+#        y2 = y0 + b * np.sqrt(1 - (x / a) ** 2)
+#
+#        # Mask lines on the backside
+#        if si != 0:
+#            if inc > np.pi / 2:
+#                ymax = y1[np.argmax(x ** 2 + y1 ** 2)]
+#                y1[y1 < ymax] = np.nan
+#                ymax = y2[np.argmax(x ** 2 + y2 ** 2)]
+#                y2[y2 < ymax] = np.nan
+#            else:
+#                ymax = y1[np.argmax(x ** 2 + y1 ** 2)]
+#                y1[y1 > ymax] = np.nan
+#                ymax = y2[np.argmax(x ** 2 + y2 ** 2)]
+#                y2[y2 > ymax] = np.nan
+#
+#        # Rotate them
+#        for y in (y1, y2):
+#            xr = x * co - y * so
+#            yr = x * so + y * co
+#            res.append((xr, yr))
+
 
     ell    = np.deg2rad(ell)
     i_star = np.deg2rad(i_star)
@@ -446,14 +492,25 @@ def plot_velocity_map(v_eq, ell, i_star=90, alpha=0, Nxy=200,
     ynorm_mark = znorm * np.sin(beta) + ynorm * np.cos(beta)
     V          = (xnorm * v_eq * np.sin(i_star) *  (1 - alpha * ynorm_mark**2))
 
+    if u is not None:
+        DELTA = np.sqrt(X**2 + Y**2)
+        MU = np.sqrt(1 - DELTA**2)
+
+        I = 1 - u[0] * (1 - MU) - u[1] * (1 - MU)**2 
+        V *= I
+
+
     theta = np.linspace(0, 2*np.pi, 4000)
     r = 1
     _x = r * np.cos(theta)
     _y = r * np.sin(theta)
 
     plt.style.use('paper')
-    fig, ax = plt.subplots()#figsize=(6,6))
-    ax.plot(_x, _y, 'black', lw=1.0, rasterized=True)
+    fig, ax = plt.subplots(figsize=figsize)#figsize=(6,6))
+    ax.plot(_x, _y, 'black', lw=1.5, rasterized=True)
+
+#    for xlat, ylat in res:
+#        ax.plot(xlat, ylat, lw=0.5, c='k', alpha=0.6)
 
 #    from matplotlib import cm
     cmap = cm.get_cmap("RdBu_r")
@@ -462,7 +519,8 @@ def plot_velocity_map(v_eq, ell, i_star=90, alpha=0, Nxy=200,
     if vmax is None:
         vmax = V[~np.isnan(V)].max()
 
-    norm = colors.DivergingNorm(vmin=vmin, vcenter=0., vmax=vmax)
+#    norm = colors.DivergingNorm(vmin=vmin, vcenter=0., vmax=vmax)
+    norm = colors.TwoSlopeNorm(vmin=vmin, vcenter=0., vmax=vmax)
 
     # RdBu_r, coolwarm, bwr, seismic
 