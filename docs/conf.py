b9ee59d1992cfc3d0253590f858534c228702f4e
Vedad Kunovac <5363768+vedad@users.noreply.github.com>
2024-12-17 15:52:35 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -54,8 +54,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
-#html_style = 'css/olio.css'
+html_theme = 'alabaster'
+html_style = 'css/olio.css'
 html_sidebars = {
    '**': ['about.html', 'globaltoc.html'],
    'using/windows': ['windowssidebar.html'],
